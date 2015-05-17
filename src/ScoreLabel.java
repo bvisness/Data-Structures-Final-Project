ef002ff2063ba51fd0c78fe13e456c1e65b1e73e
Ben Visness <bvisness@gmail.com>
2015-05-17 16:23:30 -0500

    Javadocced ScoreLabel.

--- src/ScoreLabel.java
+++ src/ScoreLabel.java
@@ -2,14 +2,25 @@ import java.awt.Font;
 
 import javax.swing.JLabel;
 
-
+/**
+ * Defines a label for displaying player scores in Carcassonne.
+ * 
+ * @author Ben Visness
+ * 
+ */
 @SuppressWarnings("serial")
 public class ScoreLabel extends JLabel {
-	
+
+	/**
+	 * Creates a new ScoreLabel with the given text.
+	 * 
+	 * @param text
+	 *            The text for the label.
+	 */
 	public ScoreLabel(String text) {
 		super(text, JLabel.CENTER);
 		setOpaque(true);
 		setFont(new Font(this.getFont().getName(), Font.BOLD, 48));
 	}
-	
+
 }
