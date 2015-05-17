aff723e0da6fa0ec7d3c6cc29405c5d283bec6ce
Ben Visness <bvisness@gmail.com>
2015-05-16 22:51:52 -0500

    Javadocced my helper classes and enums.

--- src/InvalidMoveException.java
+++ src/InvalidMoveException.java
@@ -1,17 +1,43 @@
-
-
+/**
+ * An exception to be thrown if an illegal move is performed in a board game.
+ * 
+ * @author Ben Visness
+ * 
+ */
 public class InvalidMoveException extends RuntimeException {
-    
+
+	/**
+	 * Constructs a new InvalidMoveException with a message.
+	 * 
+	 * @param message
+	 *            The message for this exception.
+	 */
 	public InvalidMoveException(String message) {
-        super(message);
-    }
-	
+		super(message);
+	}
+
+	/**
+	 * Constructs a new InvalidMoveException with another Throwable object.
+	 * (StackOverflow told me to do it!)
+	 * 
+	 * @param throwable
+	 *            The other Throwable thing
+	 */
 	public InvalidMoveException(Throwable throwable) {
-        super(throwable);
-    }
-	
+		super(throwable);
+	}
+
+	/**
+	 * Constructs a new InvalidMoveException with a message and another
+	 * Throwable object. (Again, don't ask me why.)
+	 * 
+	 * @param message
+	 *            The message for this exception.
+	 * @param throwable
+	 *            The other Throwable thing.
+	 */
 	public InvalidMoveException(String message, Throwable throwable) {
 		super(message, throwable);
 	}
-	
+
 }
--- src/QuadrantType.java
+++ src/QuadrantType.java
@@ -1,13 +1,19 @@
 import java.util.Random;
 
+/**
+ * This enum defines a type of quadrant, either GRASS, ROAD, or CITY.
+ * 
+ * @author Ben Visness
+ * 
+ */
 public enum QuadrantType {
 	GRASS, ROAD, CITY;
-	
+
 	private static final QuadrantType[] VALUES = values();
 	private static final int SIZE = VALUES.length;
 	private static final Random RANDOM = new Random();
-	
-	public static QuadrantType randomType()  {
+
+	public static QuadrantType randomType() {
 		return VALUES[RANDOM.nextInt(SIZE)];
 	}
 };
